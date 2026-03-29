33145a479f02c9327a62b4e2d761fd904050051d
luckychauhan-ai <luckyiitbombay@gmail.com>
2026-03-29 17:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if not st.session_state.logged_in:
         st.write("💡 Save energy efficiently")
 
         # Better logo
-    st.image("https://cdn-icons-png.flaticon.com/512/1146/1146869.png", width=180)        # RIGHT SIDE
+st.image("https://cdn-icons-png.flaticon.com/512/1041/1041916.png", width=180)# RIGHT SIDE
     with col2:
         st.subheader("🔐 Login")
 
